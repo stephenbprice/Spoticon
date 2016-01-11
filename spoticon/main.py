63e32e3f97bc891b9951aeaca0bdf56100621ac2
Stephen Price <stevepri87@gmail.com>
2016-01-11 00:49:08 -0600

    Add song queue

--- spoticon/main.py
+++ spoticon/main.py
@@ -1,6 +1,11 @@
 import subprocess
+import time
 import curses
 import spotipy, json
+import threading
+
+from operator import attrgetter
+
 
 class Spoticon(object):
 
@@ -9,6 +14,11 @@ class Spoticon(object):
 
         #Default curses screen
         self.stdScr = stdScr
+
+        #Variables for general playback
+        self.nowPlaying = None
+        self.repeatOneSong = False
+        self.pauseCount = 0
 
         #Initial curses setup
         curses.noecho()
@@ -26,6 +36,12 @@ class Spoticon(object):
         self.backHistory = []
         self.forwardHistory = []
 
+        #Variables for playlists
+        self.playlist = []
+        self.currentPlaylistIndex = -1
+        self.playerListenerThread = threading.Thread(target = self.listen_for_song_advance)
+        self.closePlayerListener = False
+
         #Variables for Scrolling Screen
         self.topLineNum = 0
         self.highlightLineNum = 0
@@ -42,7 +58,9 @@ class Spoticon(object):
 
     def run(self):
         self.show_intro()
+        self.start_player_listener_thread()
         self.listen_for_commands()
+
 
 
     def show_intro(self):
@@ -51,7 +69,6 @@ class Spoticon(object):
         ''' 
 
         self.mainScr.addstr(1, 1, introScreen)
-        self.mainScr.refresh()
 
 
     def listen_for_commands(self):
@@ -60,21 +77,42 @@ class Spoticon(object):
             charInput = self.stdScr.getch()
 
             if (charInput == ord('q')):
+                self.stop_player_listener_thread()
                 curses.endwin()
                 break
             elif (charInput == ord('s')):
                 self.search_content()
             elif (charInput == ord('\n')):
                 line = self.results[self.highlightLineNum]
-                self.play_song(line.get('song_uri'))
+                self.play_song(line)
+            elif (charInput == ord(' ')):
+                self.play_pause()
             elif (charInput == ord('j')):
                 self.updown(1)
             elif (charInput == ord('k')):
                 self.updown(-1)
+            elif (charInput == ord('l')):
+                self.forward_history();
+            elif (charInput == ord('h')):
+                self.back_history();
+            elif (charInput == ord('r')):
+                self.toggle_repeat_one_song()
+            elif (charInput == ord('L')):
+                self.playlist_play_next()
+            elif (charInput == ord('H')):
+                self.playlist_play_last()
+            elif (charInput == ord('C')):
+                self.clear_playlist()
+            elif (charInput == ord('+')):
+                line = self.results[self.highlightLineNum]
+                self.add_song_to_playlist(line)
+            elif (charInput == ord('A')):
+                self.add_all_songs_to_playlist()
 
     def get_input(self, prompt):
         curses.curs_set(1)
-        searchScr = self.stdScr.derwin(20, 20, 20, 20)
+        searchScr = self.stdScr.derwin(5, 40, 20, 20)
+        searchScr.refresh()
         searchScr.addstr(0, 0, prompt)
         curses.echo()
         usrInput = searchScr.getstr().decode(encoding='utf-8')
@@ -92,13 +130,56 @@ class Spoticon(object):
 
         if (len(searchStr) > 2):
             if (self.results):
-                self.back_history.append(self.results)
-            searchResults = self.spotify.search(searchStr)
+                self.backHistory.append(self.results)
+                self.topLineNum = 0
+                self.highlightLineNum = 0
+            searchResults = self.spotify.search(searchStr, limit=50)
             self.results = self.parse_spotify_results(searchResults)
 
         if(self.results):
             self.display_main_screen()
 
+    
+    def forward_history(self):
+        if(self.forwardHistory):
+            self.backHistory.append(self.results)
+            self.results = self.forwardHistory.pop()
+            self.display_main_screen()
+
+
+    def back_history(self):
+        if (self.backHistory):
+            self.forwardHistory.append(self.results)
+            self.results = self.backHistory.pop()
+            self.display_main_screen()
+
+
+    def toggle_repeat_one_song(self):
+        self.repeatOneSong = not self.repeatOneSong
+
+
+    def add_song_to_playlist(self, song):
+        self.playlist.append(song)
+    
+
+    def add_all_songs_to_playlist(self):
+        if self.results:
+            self.playlist += self.results
+        
+    def clear_playlist(self):
+        self.playlist = []
+        self.currentPlaylistIndex = -1
+
+
+    def playlist_play_next(self):
+        if (self.currentPlaylistIndex < len(self.playlist) - 1):
+            self.currentPlaylistIndex += 1
+            self.play_song(self.playlist[self.currentPlaylistIndex])
+
+    def playlist_play_last(self):
+        if(self.currentPlaylistIndex > 0 and self.currentPlaylistIndex < len(self.playlist)):
+            self.currentPlaylistIndex -= 1
+            self.play_song(self.playlist[self.currentPlaylistIndex])
     
     #Scrolling code pulled from Lyle Scott's Python-curses-Scrolling-Example
     #https://github.com/LyleScott/Python-curses-Scrolling-Example/
@@ -106,7 +187,7 @@ class Spoticon(object):
         self.mainScr.erase()
         top = self.topLineNum
         bottom = self.topLineNum + self.mainScrHeight
-        for (index,line,) in enumerate(self.results):
+        for (index,line,) in enumerate(self.results[top:bottom]):
             linenum = self.topLineNum + index
             lineString = self.format_line(line)
 
@@ -125,6 +206,7 @@ class Spoticon(object):
             return
         elif increment == 1 and nextLineNum == self.mainScrHeight and (self.topLineNum + self.mainScrHeight) != len(self.results):
             self.topLineNum += 1
+            return
 
         #Scroll highlight line
         if increment == -1 and (self.topLineNum != 0 or self.highlightLineNum != 0):
@@ -144,13 +226,67 @@ class Spoticon(object):
             album_name = track['album']['name']
             artist_name = track['artists'][0]['name']
             song_uri = track['uri']
-            res.append({'song_name': song_name, 'album_name': album_name, 'artist_name': artist_name, 'song_uri': song_uri})
-        return res
+            popularity = track['popularity']
+            res.append({'song_name': song_name, 'album_name': album_name, 'artist_name': artist_name, 'song_uri': song_uri, 'popularity': popularity})
+        return self.sort_results(res)
+
+    
+    def sort_results(self, results):
+        return sorted(results, key = lambda k: k['popularity'], reverse = True)
 
 
-    def play_song(self, song_uri):
-        appleScript = ['osascript', '-e', 'tell application "Spotify" to play track "{0}"'.format(song_uri)]
+    def start_player_listener_thread(self):
+        self.playerListenerThread.setDaemon(True)
+        self.playerListenerThread.start()
+
+
+    def stop_player_listener_thread(self):
+        self.closePlayerListener = True
+
+
+    def listen_for_song_advance(self):
+        while not self.closePlayerListener:
+            playerState = self.get_player_state()
+            playerPosition = self.get_player_position()
+            if (playerState == 'paused' or playerState == 'stopped') and playerPosition == '0.0':
+                if self.nowPlaying and self.repeatOneSong:
+                    self.play_song(self.nowPlaying)
+                elif self.playlist:
+                    if self.pauseCount >= 2:
+                        self.playlist_play_next()
+                        self.pauseCount = 0
+                    else:
+                        self.pauseCount += 1
+            else:
+                self.pauseCount = 0
+            time.sleep(0.5)
+
+
+    def play_song(self, song):
+        self.nowPlaying = song
+        appleScript = ['osascript', '-e', 'tell application "Spotify" to play track "{0}"'.format(song.get('song_uri'))]
         subprocess.call(appleScript)
+
+    def play_pause(self):
+        appleScript = ['osascript', '-e', 'tell application "Spotify" to playpause']
+        subprocess.call(appleScript)
+    
+    def get_player_state(self):
+        appleScript = ['osascript', '-e', 'tell application "Spotify" \n get player state \n end tell']
+        playerState = subprocess.check_output(appleScript)
+        return self.bytes_to_string(playerState)
+
+    def get_player_position(self):
+        appleScript = ['osascript', '-e', 'tell application "Spotify" \n get player position \n end tell']
+        playerPosition = subprocess.check_output(appleScript)
+        return self.bytes_to_string(playerPosition)
+
+
+    def bytes_to_string(self, b):
+        convertedString = b.decode('utf-8').rstrip()
+        return convertedString
+
+
 
 def run():
     curses.wrapper(Spoticon)
